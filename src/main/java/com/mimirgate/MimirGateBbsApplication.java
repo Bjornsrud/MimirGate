bcb98b853179d0c001076dd1955f4c6cf4ce4314
Bjornsrud <ckb78@outlook.com>
2025-09-11 12:28:45 +0200

    Add basic spring booty project.

---
+++ src/main/java/com/mimirgate/MimirGateBbsApplication.java
@@ -0,0 +1,13 @@
+package com.mimirgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimirGateBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimirGateBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mimirgate/MimirGateBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimirgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimirGateBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
